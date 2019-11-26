af0d735148ae99e735a2aaa5facf59bdb8eb305b
Michael Milke <desmilke@gmail.com>
2019-11-26 19:18:08 +0100

    challenges

--- JS-a/index.js
+++ JS-a/index.js
@@ -1,4 +1,36 @@
 "use strict"
-let sound = "car is driving";
+// /**
+//  * EXAMPLE 1 - Variable declaration
+//  */
+// var a;
 
-console.log(sound);
+// console.log(a);
+
+// // console.log(b); Uncaught ReferenceError: b is not defined
+
+// let c;
+
+/**
+ *  EXAMPLE 2 - "let"
+ */
+let myNumber = 10;
+
+console.log(myNumber);
+
+/**
+ * CHALLENGE 1 +
+ */
+
+ /**
+  * CHALLENGE 2
+  */
+
+  let myObject = { a: 10,
+                    b: true,
+                 __proto__: Object };
+   let anotherObject = { newA: 0, b: true, __proto__: Object};
+
+   console.log(myObject);
+   console.log(anotherObject);
+   
+   
